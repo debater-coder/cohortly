8e1a0f1551dca8fb92af9828e3a94f90573a7b7e
debater-coder <hamzah@syedahmed.net>
2026-07-19 16:19:01 +1000

    feat: breadcrumbs

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = "dashboard"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.Index.as_view(), name="index"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from subjects.models import Subject
 
 
-def index(request: HttpRequest):
-    return render(request, "dashboard/index.html")
+class Index(ListView):
+    model = Subject
+    context_object_name = "subjects"
+    template_name = "dashboard/index.html"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        qs = qs.filter(members=self.request.user)
+
+        return qs

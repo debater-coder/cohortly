4e54a210a868461cee7cf43d1a2b761ec3ca6518
debater-coder <hamzah@syedahmed.net>
2026-08-30 13:56:05 +1000

    fix

--- cohortly/settings.py
+++ cohortly/settings.py
@@ -194,6 +194,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
